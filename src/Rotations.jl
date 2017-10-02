248042235264ea856023ab501d1bfb671dfcf5d8
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2017-10-02 13:47:29 -0300

    Move the definition of Euler Angles to its file
    The definition of the type `EulerAngles` was moved to the file
    `euler_angles.jl`. Hence, the file `types.jl` was deleted.

--- src/Rotations.jl
+++ src/Rotations.jl
@@ -2,11 +2,10 @@ VERSION >= v"0.4.0-dev+6521" && __precompile__()
 
 module Rotations
 
-export EulerAngles, RotationSequenceError
+export RotationSequenceError
 
 import Base: sin, cos
 
-include("types.jl")
 include("exceptions.jl")
 
 include("DCM.jl")
--- src/euler_angles.jl
+++ src/euler_angles.jl
@@ -2,7 +2,28 @@
 #                                 Euler Angles
 ################################################################################
 
+export EulerAngles
 export angle2dcm, angle2dcm!
+
+################################################################################
+#                                    Types
+################################################################################
+
+"""
+### type EulerAngles where T<: Real
+
+The definition of Euler Angles, which is composed of three angles `a1`, `a2`,
+and `a3` together with a rotation sequence `rot_seq`. The latter is provided by
+a string with three characters, each one indicating the rotation axis of the
+corresponding angle.
+"""
+
+type EulerAngles{T<:Real}
+    a1::T
+    a2::T
+    a3::T
+    rot_seq::AbstractString
+end
 
 ################################################################################
 #                                  Functions
