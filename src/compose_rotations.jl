e31c835141645795aa822f55846525d6563b1fe8
ronisbr <ronisbr@gmail.com>
2018-05-12 14:05:38 -0300

    :wrench: Improve compose_rotation algorithm
    The algorithm was improved based on @Per suggestion in
    
        https://discourse.julialang.org/t/improve-the-performance-of-multiplication-of-an-arbitrary-number-of-matrices/10835/24

--- src/compose_rotations.jl
+++ src/compose_rotations.jl
@@ -8,7 +8,7 @@ export compose_rotation
 # ==============================================================================
 
 """
-### @inline function compose_rotation(R1, R2 [, R3, R4, R5, ...])
+### @inline function compose_rotation(R1, [, R2, R3, R4, R5, ...])
 
 Compute a composed rotation using the rotations `R1`, `R2`, `R3`, `R4`, ..., in
 the following order:
@@ -30,67 +30,20 @@ The output will have the same type as the inputs (DCM or quaternion).
 ##### Args
 
 * R1: First rotation (DCM or quaternion).
-* R2: Second rotation (DCM or quaternion).
-* R3, R4, R5, ...: (OPTIONAL) Other rotations (DCMs or quaternions).
+* R2, R3, R4, R5, ...: (OPTIONAL) Other rotations (DCMs or quaternions).
 
 ##### Returns
 
 The composed rotation.
 
 """
+# This algorithm was proposed by @Per in
+#
+#   https://discourse.julialang.org/t/improve-the-performance-of-multiplication-of-an-arbitrary-number-of-matrices/10835/24
+@inline compose_rotation(D::SMatrix{3,3}) = D
+@inline compose_rotation(D::SMatrix{3,3}, Ds::SMatrix{3,3}...) =
+    compose_rotation(Ds...)*D
 
-# TODO: It turns out that after 5 multiplications, the function with a `for`
-# performs better in a specific computer. This needs further verification.
-
-@inline compose_rotation(D1::SMatrix{3,3},
-                         D2::SMatrix{3,3}) = D2*D1
-@inline compose_rotation(D1::SMatrix{3,3},
-                         D2::SMatrix{3,3},
-                         D3::SMatrix{3,3}) = D3*D2*D1
-@inline compose_rotation(D1::SMatrix{3,3},
-                         D2::SMatrix{3,3},
-                         D3::SMatrix{3,3},
-                         D4::SMatrix{3,3}) =
-    D4*D3*D2*D1
-@inline compose_rotation(D1::SMatrix{3,3},
-                         D2::SMatrix{3,3},
-                         D3::SMatrix{3,3},
-                         D4::SMatrix{3,3},
-                         D5::SMatrix{3,3}) =
-    D5*D4*D3*D2*D1
-
-@inline function compose_rotation(D1::SMatrix{3,3}, D2::SMatrix{3,3}, Ds::SMatrix{3,3}...)
-    result = D2*D1
-
-    for Di in Ds
-        result = Di*result
-    end
-
-    result
-end
-
-@inline compose_rotation(q1::Quaternion, q2::Quaternion) = q1*q2
-@inline compose_rotation(q1::Quaternion,
-                         q2::Quaternion,
-                         q3::Quaternion) = q1*q2*q3
-@inline compose_rotation(q1::Quaternion,
-                         q2::Quaternion,
-                         q3::Quaternion,
-                         q4::Quaternion) = q1*q2*q3*q4
-@inline compose_rotation(q1::Quaternion,
-                         q2::Quaternion,
-                         q3::Quaternion,
-                         q4::Quaternion,
-                         q5::Quaternion) = q1*q2*q3*q4*q5
-
-@inline function compose_rotation(q1::Quaternion,
-                                  q2::Quaternion,
-                                  qs::Quaternion...)
-    result = q1*q2
-
-    for qi in qs
-        result = result*qi
-    end
-
-    result
-end
+@inline compose_rotation(q::Quaternion) = q
+@inline compose_rotation(q::Quaternion, qs::Quaternion...) =
+    q*compose_rotation(qs...)
