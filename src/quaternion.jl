4957ab39d8eb5a289dbd3e0d406240c68af8a1f3
ronisbr <ronisbr@gmail.com>
2018-08-08 23:06:11 -0300

    :wrench: Export eye because of Julia v1.0
    `eye` has been removed in Julia v1.0. Hence, we export `eye` as a
    temporary fix in Julia v1.0. This must be improved.
    
    See issue #5

--- src/quaternion.jl
+++ src/quaternion.jl
@@ -2,7 +2,7 @@
 #                                 Quaternions
 ################################################################################
 
-export dquat, quat2angle, quat2angleaxis, quat2dcm, vect
+export dquat, eye, quat2angle, quat2angleaxis, quat2dcm, vect
 
 ################################################################################
 #                                 Initializers
