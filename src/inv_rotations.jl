8f6f3e3c41af56c5714679b24040aac05462589c
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-05-16 22:45:00 -0300

    :art: Apply BlueStyle to inv_rotations.jl

--- src/inv_rotations.jl
+++ src/inv_rotations.jl
@@ -1,3 +1,12 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Description
+# ==============================================================================
+#
+#   Generic function to inverse rotations.
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 export inv_rotation
 
 ################################################################################
@@ -14,37 +23,37 @@ Compute the inverse rotation of `R`, which can be:
 * A set of Euler anlges (`EulerAngles`); or
 * A quaternion (`Quaternion`).
 
-The output will have the same type as `R` (DCM or quaternion).
+The output will have the same type as `R`.
 
-# Remarks
+!!! note
+    If `R` is a DCM, than its transpose is computed instead of its inverse to
+    reduce the computational burden. The both are equal if the DCM has unit
+    norm. This must be verified by the user.
 
-If `R` is a DCM, than its transpose is computed instead of its inverse to reduce
-the computational burden. The both are equal if the DCM has unit norm. This must
-be verified by the user.
-
-If `R` is a quaternion, than its conjugate is computed instead of its inverse to
-reduce the computational burden. The both are equal if the quaternion has unit
-norm. This must be verified by the used.
+!!! note
+    If `R` is a quaternion, than its conjugate is computed instead of its
+    inverse to reduce the computational burden. The both are equal if the
+    quaternion has unit norm. This must be verified by the used.
 
 # Example
 
 ```julia-repl
-julia> D = angle_to_dcm(+pi/3,+pi/4,+pi/5,:ZYX);
+julia> D = angle_to_dcm(pi / 3, pi / 4, pi / 5, :ZYX);
 
 julia> inv_rotation(D)
-3×3 StaticArrays.SArray{Tuple{3,3},Float64,2,9}:
+3×3 StaticArrays.SMatrix{3, 3, Float64, 9} with indices SOneTo(3)×SOneTo(3):
   0.353553  -0.492816  0.795068
   0.612372   0.764452  0.201527
  -0.707107   0.415627  0.572061
 
-julia> ea = EulerAngleAxis(30*pi/180, [1;0;0]);
+julia> ea = EulerAngleAxis(30 * pi / 180, [1, 0, 0]);
 
 julia> inv_rotation(ea)
 EulerAngleAxis{Float64}:
   Euler angle:   0.5236 rad ( 30.0000 deg)
    Euler axis: [ -1.0000,  -0.0000,  -0.0000]
 
-julia> Θ = EulerAngles(-pi/3, -pi/2, -pi, :YXZ);
+julia> Θ = EulerAngles(-pi / 3, -pi / 2, -pi, :YXZ);
 
 julia> inv_rotation(Θ)
 EulerAngles{Float64}:
@@ -52,13 +61,12 @@ EulerAngles{Float64}:
   R(X):   1.5708 rad (  90.0000 deg)
   R(Y):   1.0472 rad (  60.0000 deg)
 
-julia> q = angle_to_quat(+pi/3,+pi/4,+pi/5,:ZYX);
+julia> q = angle_to_quat(pi / 3, pi / 4, pi / 5, :ZYX);
 
 julia> inv_rotation(q)
 Quaternion{Float64}:
   + 0.8200711519756747 - 0.06526868310243991.i - 0.45794027732580056.j - 0.336918398289752.k
 ```
-
 """
 @inline inv_rotation(D::DCM) = D'
 @inline inv_rotation(ea::EulerAngleAxis) = inv(ea)
