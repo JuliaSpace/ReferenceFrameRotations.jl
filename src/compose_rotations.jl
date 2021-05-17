0ea3ac2c87f2a969d636a838fe01a32a083e903b
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-05-16 22:46:40 -0300

    :art: Apply BlueStype to compose_rotations.jl

--- src/compose_rotations.jl
+++ src/compose_rotations.jl
@@ -42,19 +42,19 @@ The output will have the same type as the inputs.
 # Example
 
 ```julia-repl
-julia> D1 = angle_to_dcm(+pi/3, +pi/4, +pi/5, :ZYX);
+julia> D1 = angle_to_dcm(pi / 3, pi / 4, pi / 5, :ZYX);
 
-julia> D2 = angle_to_dcm(-pi/5, -pi/4, -pi/3, :XYZ);
+julia> D2 = angle_to_dcm(-pi / 5, -pi / 4, -pi / 3, :XYZ);
 
 julia> compose_rotation(D1, D2)
-3×3 StaticArrays.SArray{Tuple{3,3},Float64,2,9}:
- 1.0          0.0          5.55112e-17
- 0.0          1.0          5.55112e-17
- 5.55112e-17  5.55112e-17  1.0
+3×3 StaticArrays.SMatrix{3, 3, Float64, 9} with indices SOneTo(3)×SOneTo(3):
+ 1.0          1.08801e-17  3.54837e-17
+ 1.08801e-17  1.0          2.88714e-17
+ 3.54837e-17  2.88714e-17  1.0
 
-julia> ea1 = EulerAngleAxis(30*pi/180, [0, 1, 0]);
+julia> ea1 = EulerAngleAxis(30 * pi / 180, [0, 1, 0]);
 
-julia> ea2 = EulerAngleAxis(45*pi/180, [0, 1, 0]);
+julia> ea2 = EulerAngleAxis(45 * pi / 180, [0, 1, 0]);
 
 julia> compose_rotation(ea1, ea2)
 EulerAngleAxis{Float64}:
@@ -71,9 +71,9 @@ EulerAngles{Float64}:
   R(Y):   0.0000 rad (   0.0000 deg)
   R(Z):  -0.0000 rad (  -0.0000 deg)
 
-julia> q1 = angle_to_quat(+pi/3, +pi/4, +pi/5, :ZYX);
+julia> q1 = angle_to_quat(pi / 3, pi / 4, pi / 5, :ZYX);
 
-julia> q2 = angle_to_quat(-pi/5, -pi/4, -pi/3, :XYZ);
+julia> q2 = angle_to_quat(-pi / 5, -pi / 4, -pi / 3, :XYZ);
 
 julia> compose_rotation(q1, q2)
 Quaternion{Float64}:
