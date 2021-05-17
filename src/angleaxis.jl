6da568b7dc45bc125763d3dbe2af6ebc4bc25fc5
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-05-16 21:01:56 -0300

    :arg: Apply BlueStyle to angleaxis.jl

--- src/angleaxis.jl
+++ src/angleaxis.jl
@@ -74,7 +74,7 @@ function *(av₂::EulerAngleAxis{T1}, av₁::EulerAngleAxis{T2}) where {T1, T2}
             s = -1
         end
 
-        v = s * (sθ₁o2 * cθ₂o2*v₁ + cθ₁o2 * sθ₂o2 * v₂ + sθ₁o2 * sθ₂o2 * (v₁ × v₂)) / sθo2
+        v = s * (sθ₁o2 * cθ₂o2 * v₁ + cθ₁o2 * sθ₂o2 * v₂ + sθ₁o2 * sθ₂o2 * (v₁ × v₂)) / sθo2
 
         return EulerAngleAxis(θ, v)
     end
@@ -83,34 +83,52 @@ end
 """
     inv(av::EulerAngleAxis)
 
-Compute the inverse rotation of `ea`. The Euler angle returned by this function
-will always be in the interval [0, π].
+Compute the inverse rotation of the Euler angle and axis `av`.
 
+The Euler angle returned by this function will always be in the interval `[0, π]
+rad`.
+
+# Examples
+
+```julia-repl
+julia> av = EulerAngleAxis(deg2rad(20), [sqrt(2)/2, 0, sqrt(2)/2])
+EulerAngleAxis{Float64}:
+  Euler angle:   0.3491 rad ( 20.0000 deg)
+   Euler axis: [  0.7071,   0.0000,   0.7071]
+
+julia> inv(av)
+EulerAngleAxis{Float64}:
+  Euler angle:   0.3491 rad ( 20.0000 deg)
+   Euler axis: [ -0.7071,  -0.0000,  -0.7071]
+
+julia> av = EulerAngleAxis(deg2rad(-20), [sqrt(2)/2, 0, sqrt(2)/2])
+EulerAngleAxis{Float64}:
+  Euler angle:  -0.3491 rad (-20.0000 deg)
+   Euler axis: [  0.7071,   0.0000,   0.7071]
+
+julia> inv(av)
+EulerAngleAxis{Float64}:
+  Euler angle:   0.3491 rad ( 20.0000 deg)
+   Euler axis: [  0.7071,   0.0000,   0.7071]
+```
 """
 @inline function inv(av::EulerAngleAxis{T}) where T<:Number
     # Make sure that the Euler angle is always in the inverval [0,π]
     s = -1
-    θ = mod(av.a, T(2)*π)
+    θ = mod(av.a, T(2π))
 
     if θ > π
         s = 1
-        θ = T(2)π - θ
+        θ = T(2π) - θ
     end
 
-    EulerAngleAxis(θ, s*av.v)
+    return EulerAngleAxis(θ, s*av.v)
 end
 
 ################################################################################
 #                                      IO
 ################################################################################
 
-"""
-    display(av::EulerAngleAxis{T}) where T
-    show(io::IO, mime::MIME"text/plain", av::EulerAngleAxis{T}) where T
-
-Display in `stdout` the Euler angle and axis `ea`.
-
-"""
 function show(io::IO, av::EulerAngleAxis{T}) where T
     θ   = av.a
     v   = av.v
@@ -118,7 +136,7 @@ function show(io::IO, av::EulerAngleAxis{T}) where T
 
     print(io, "EulerAngleAxis{$T}: $str")
 
-    nothing
+    return nothing
 end
 
 function show(io::IO, mime::MIME"text/plain", av::EulerAngleAxis{T}) where T
@@ -135,7 +153,7 @@ function show(io::IO, mime::MIME"text/plain", av::EulerAngleAxis{T}) where T
     println(io, "$y  Euler angle: $d" * str_a)
       print(io, "$y   Euler axis: $d" * str_v)
 
-    nothing
+    return nothing
 end
 
 ################################################################################
@@ -149,35 +167,46 @@ end
     angleaxis_to_angle(θ::Number, v::AbstractVector, rot_seq::Symbol)
     angleaxis_to_angle(av::EulerAngleAxis, rot_seq::Symbol)
 
-Convert the Euler angle `θ` [rad]  and Euler axis `v`, which must be a unit
-vector, to Euler angles with rotation sequence `rot_seq`. Those values can also
-be passed inside the structure `ea` (see `EulerAngleAxis`).
+Convert the Euler angle `θ` [rad]  and Euler axis `v` to Euler angles with
+rotation sequence `rot_seq`.
+
+Those values can also be passed inside the structure `av` (see
+[`EulerAngleAxis`](@ref)).
 
 The rotation sequence is defined by a `:Symbol`. The possible values are:
 `:XYX`, `XYZ`, `:XZX`, `:XZY`, `:YXY`, `:YXZ`, `:YZX`, `:YZY`, `:ZXY`, `:ZXZ`,
 `:ZYX`, and `:ZYZ`. If no value is specified, then it defaults to `:ZYX`.
 
+!!! warning
+    It is expected that the vector `v` is unitary. However, no verification is
+    performed inside the function. The user must handle this situation.
+
 # Example
 
 ```julia-repl
-julia> ea = EulerAngleAxis(45*pi/180, [1;0;0]);
+julia> av = EulerAngleAxis(deg2rad(45), [1, 0, 0])
+EulerAngleAxis{Float64}:
+  Euler angle:   0.7854 rad ( 45.0000 deg)
+   Euler axis: [  1.0000,   0.0000,   0.0000]
 
-julia> angleaxis_to_angles(ea, :ZXY)
+julia> angleaxis_to_angle(av, :ZXY)
 EulerAngles{Float64}:
   R(Z):   0.0000 rad (   0.0000 deg)
   R(X):   0.7854 rad (  45.0000 deg)
   R(Y):   0.0000 rad (   0.0000 deg)
-
 ```
 """
 @inline function angleaxis_to_angle(θ::Number, v::AbstractVector, rot_seq::Symbol)
-    # First we convert to quaternions then to Euler angles.
-    quat_to_angle( angleaxis_to_quat(θ, v), rot_seq )
+    # Check the arguments.
+    (length(v) ≠ 3) && throw(ArgumentError("The provided vector for the Euler axis must have 3 elements."))
+
+    # First we convert to DCM then to Euler angles.
+    return dcm_to_angle(angleaxis_to_dcm(θ, v), rot_seq)
 end
 
-@inline angleaxis_to_angle(av::EulerAngleAxis, rot_seq::Symbol) =
-    angleaxis_to_angle(av.a, av.v, rot_seq)
-
+@inline function angleaxis_to_angle(av::EulerAngleAxis, rot_seq::Symbol)
+    return angleaxis_to_angle(av.a, av.v, rot_seq)
+end
 
 # DCM
 # ==============================================================================
@@ -186,45 +215,49 @@ end
     angleaxis_to_dcm(a::Number, v::AbstractVector)
     angleaxis_to_dcm(av::EulerAngleAxis)
 
-Convert the Euler angle `a` [rad] and Euler axis `v`, which must be a unit
-vector to a DCM. Those values can also be passed inside the structure `ea` (see
-`EulerAngleAxis`).
+Convert the Euler angle `a` [rad] and Euler axis `v` to a DCM.
 
-# Remarks
+Those values can also be passed inside the structure `ea` (see
+[`EulerAngleAxis`](@ref)).
 
-It is expected that the vector `v` is unitary. However, no verification is
-performed inside the function. The user must handle this situation.
+!!! warning
+    It is expected that the vector `v` is unitary. However, no verification is
+    performed inside the function. The user must handle this situation.
 
 # Example
 
 ```julia-repl
-julia> v = [1;1;1];
+julia> v = [1, 1, 1];
 
 julia> v /= norm(v);
 
-julia> angleaxis_to_dcm(pi/2,v)
-3×3 StaticArrays.SArray{Tuple{3,3},Float64,2,9}:
+julia> angleaxis_to_dcm(pi / 2, v)
+3×3 StaticArrays.SMatrix{3, 3, Float64, 9} with indices SOneTo(3)×SOneTo(3):
   0.333333   0.910684  -0.244017
  -0.244017   0.333333   0.910684
   0.910684  -0.244017   0.333333
 
-julia> ea = EulerAngleAxis(pi/2,v);
+julia> ea = EulerAngleAxis(pi / 2, v);
 
 julia> angleaxis_to_dcm(ea)
-3×3 StaticArrays.SArray{Tuple{3,3},Float64,2,9}:
+3×3 StaticArrays.SMatrix{3, 3, Float64, 9} with indices SOneTo(3)×SOneTo(3):
   0.333333   0.910684  -0.244017
  -0.244017   0.333333   0.910684
   0.910684  -0.244017   0.333333
 ```
-
 """
 @inline function angleaxis_to_dcm(a::Number, v::AbstractVector)
+    # Check the arguments.
+    (length(v) ≠ 3) && throw(ArgumentError("The provided vector for the Euler axis must have 3 elements."))
+
     sθ, cθ = sincos(a)
     aux    = 1 - cθ
 
-    DCM(  cθ + v[1]*v[1]*aux   , v[1]*v[2]*aux + v[3]*sθ, v[1]*v[3]*aux - v[2]*sθ,
-        v[1]*v[2]*aux - v[3]*sθ,    cθ + v[2]*v[2]*aux  , v[2]*v[3]*aux + v[1]*sθ,
-        v[1]*v[3]*aux + v[2]*sθ, v[2]*v[3]*aux - v[1]*sθ,   cθ + v[3]*v[3]*aux)'
+    return DCM(
+          cθ + v[1] * v[1] * aux,      v[1] * v[2] * aux + v[3] * sθ, v[1] * v[3] * aux - v[2] * sθ,
+        v[1] * v[2] * aux - v[3] * sθ,   cθ + v[2] * v[2] * aux,      v[2] * v[3] * aux + v[1] * sθ,
+        v[1] * v[3] * aux + v[2] * sθ, v[2] * v[3] * aux - v[1] * sθ,   cθ + v[3] * v[3] * aux
+    )'
 end
 
 @inline angleaxis_to_dcm(av::EulerAngleAxis) = angleaxis_to_dcm(av.a, av.v)
@@ -234,14 +267,16 @@ end
 
 """
     angleaxis_to_quat(θ::Number, v::AbstractVector)
+    angleaxis_to_quat(angleaxis::EulerAngleAxis)
 
-Convert the Euler angle `θ` [rad] and Euler axis `v`, which must be a unit
-vector, to a quaternion.
+Convert the Euler angle `θ` [rad] and Euler axis `v` to a quaternion.
 
-# Remarks
+Those values can also be passed inside the structure `ea` (see
+[`EulerAngleAxis`](@ref)).
 
-It is expected that the vector `v` is unitary. However, no verification is
-performed inside the function. The user must handle this situation.
+!!! warning
+    It is expected that the vector `v` is unitary. However, no verification is
+    performed inside the function. The user must handle this situation.
 
 # Example
 
@@ -254,45 +289,18 @@ julia> angleaxis_to_quat(pi/2,v)
 Quaternion{Float64}:
   + 0.7071067811865476 + 0.408248290463863.i + 0.408248290463863.j + 0.408248290463863.k
 ```
-
 """
-function angleaxis_to_quat(θ::Number, v::AbstractVector)
+@inline function angleaxis_to_quat(θ::Number, v::AbstractVector)
     # Check the arguments.
-    (length(v) > 3) && throw(ArgumentError("The provided vector for the Euler axis must have 3 elements."))
+    (length(v) ≠ 3) && throw(ArgumentError("The provided vector for the Euler axis must have 3 elements."))
 
-    cθo2 = cos(θ/2)
+    sθo2, cθo2 = sincos(θ / 2)
 
     # Keep `q0` positive.
     s = (cθo2 < 0) ? -1 : +1
 
     # Create the quaternion.
-    Quaternion( s*cos(θ/2), s*sin(θ/2)*v )
+    return Quaternion(s * cθo2, s * sθo2 * v)
 end
 
-"""
-    angleaxis_to_quat(angleaxis::EulerAngleAxis)
-
-Convert a Euler angle and Euler axis `angleaxis` (see `EulerAngleAxis`) to a
-quaternion.
-
-# Remarks
-
-It is expected that the vector `angleaxis.v` is unitary. However, no
-verification is performed inside the function. The user must handle this
-situation.
-
-# Example
-
-```julia-repl
-julia> v = [1;1;1];
-
-julia> v /= norm(v);
-
-julia> angleaxis_to_quat(EulerAngleAxis(pi/2,v))
-Quaternion{Float64}:
-  + 0.7071067811865476 + 0.408248290463863.i + 0.408248290463863.j + 0.408248290463863.k
-```
-
-"""
-angleaxis_to_quat(angleaxis::EulerAngleAxis) = angleaxis_to_quat(angleaxis.a,
-                                                                 angleaxis.v)
+@inline angleaxis_to_quat(av::EulerAngleAxis) = angleaxis_to_quat(av.a, av.v)
