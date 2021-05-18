ff85c6583923e6b8714ac465083f5b23463714cb
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-05-18 13:01:30 -0300

    :rotating_light: Apply BlueStyle to test quaternion_to_dcm

--- test/conversions/quaternion_to_dcm.jl
+++ test/conversions/quaternion_to_dcm.jl
@@ -29,15 +29,15 @@
     q2 = q.q2
     q3 = q.q3
 
-    @test D[1, 1] ≈ q0^2+q1^2-q2^2-q3^2
-    @test D[1, 2] ≈ 2(q1*q2+q0*q3)
-    @test D[1, 3] ≈ 2(q1*q3-q0*q2)
-    @test D[2, 1] ≈ 2(q1*q2-q0*q3)
-    @test D[2, 2] ≈ q0^2-q1^2+q2^2-q3^2
-    @test D[2, 3] ≈ 2(q2*q3+q0*q1)
-    @test D[3, 1] ≈ 2(q1*q3+q0*q2)
-    @test D[3, 2] ≈ 2(q2*q3-q0*q1)
-    @test D[3, 3] ≈ q0^2-q1^2-q2^2+q3^2
+    @test D[1, 1] ≈ q0^2 + q1^2 - q2^2 - q3^2
+    @test D[1, 2] ≈ 2(q1 * q2 + q0 * q3)
+    @test D[1, 3] ≈ 2(q1 * q3 - q0 * q2)
+    @test D[2, 1] ≈ 2(q1 * q2 - q0 * q3)
+    @test D[2, 2] ≈ q0^2 - q1^2 + q2^2 - q3^2
+    @test D[2, 3] ≈ 2(q2 * q3 + q0 * q1)
+    @test D[3, 1] ≈ 2(q1 * q3 + q0 * q2)
+    @test D[3, 2] ≈ 2(q2 * q3 - q0 * q1)
+    @test D[3, 3] ≈ q0^2 - q1^2 - q2^2 + q3^2
 end
 
 @testset "Quaternion => DCM (Float32)" begin
@@ -56,13 +56,13 @@ end
     q2 = q.q2
     q3 = q.q3
 
-    @test D[1, 1] ≈ q0^2+q1^2-q2^2-q3^2
-    @test D[1, 2] ≈ 2(q1*q2+q0*q3)
-    @test D[1, 3] ≈ 2(q1*q3-q0*q2)
-    @test D[2, 1] ≈ 2(q1*q2-q0*q3)
-    @test D[2, 2] ≈ q0^2-q1^2+q2^2-q3^2
-    @test D[2, 3] ≈ 2(q2*q3+q0*q1)
-    @test D[3, 1] ≈ 2(q1*q3+q0*q2)
-    @test D[3, 2] ≈ 2(q2*q3-q0*q1)
-    @test D[3, 3] ≈ q0^2-q1^2-q2^2+q3^2
+    @test D[1, 1] ≈ q0^2 + q1^2 - q2^2 - q3^2
+    @test D[1, 2] ≈ 2(q1 * q2 + q0 * q3)
+    @test D[1, 3] ≈ 2(q1 * q3 - q0 * q2)
+    @test D[2, 1] ≈ 2(q1 * q2 - q0 * q3)
+    @test D[2, 2] ≈ q0^2 - q1^2 + q2^2 - q3^2
+    @test D[2, 3] ≈ 2(q2 * q3 + q0 * q1)
+    @test D[3, 1] ≈ 2(q1 * q3 + q0 * q2)
+    @test D[3, 2] ≈ 2(q2 * q3 - q0 * q1)
+    @test D[3, 3] ≈ q0^2 - q1^2 - q2^2 + q3^2
 end
