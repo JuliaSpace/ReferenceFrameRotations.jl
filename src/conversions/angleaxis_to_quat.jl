ad571dccd4d9ece554c49273ab27ba879c932c0a
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2021-09-27 21:35:03 -0300

    :wrench: Improve type promotion in angleaxis_to_quat

--- src/conversions/angleaxis_to_quat.jl
+++ src/conversions/angleaxis_to_quat.jl
@@ -29,22 +29,25 @@ julia> v = [1, 1, 1];
 
 julia> v /= norm(v);
 
-julia> angleaxis_to_quat(pi/2,v)
+julia> angleaxis_to_quat(pi / 2, v)
 Quaternion{Float64}:
   + 0.707107 + 0.408248⋅i + 0.408248⋅j + 0.408248⋅k
 ```
 """
-@inline function angleaxis_to_quat(θ::Number, v::AbstractVector)
+@inline function angleaxis_to_quat(θ::T1, v::AbstractVector{T2}) where {T1, T2}
     # Check the arguments.
     (length(v) ≠ 3) && throw(ArgumentError("The provided vector for the Euler axis must have 3 elements."))
 
-    sθo2, cθo2 = sincos(θ / 2)
+    # Obtain the type of the output.
+    T = float(promote_type(T1, T2))
+
+    sθo2, cθo2 = sincos(T(θ) / 2)
 
     # Keep `q0` positive.
     s = (cθo2 < 0) ? -1 : +1
 
     # Create the quaternion.
-    return Quaternion(s * cθo2, s * sθo2 * v)
+    return Quaternion(s * cθo2, s * sθo2 * T.(v))
 end
 
 @inline angleaxis_to_quat(av::EulerAngleAxis) = angleaxis_to_quat(av.a, av.v)
