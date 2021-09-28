80a902e07fb45b9c15e3ebfff28a203ed1e683b8
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2021-09-27 21:44:08 -0300

    :lipstick: Improve code style

--- src/conversions/quat_to_dcm.jl
+++ src/conversions/quat_to_dcm.jl
@@ -34,8 +34,8 @@ function quat_to_dcm(q::Quaternion)
     q3 = q.q3
 
     return DCM(
-        q0^2+q1^2-q2^2-q3^2,   2(q1*q2+q0*q3)   ,   2(q1*q3-q0*q2),
-          2(q1*q2-q0*q3)   , q0^2-q1^2+q2^2-q3^2,   2(q2*q3+q0*q1),
-          2(q1*q3+q0*q2)   ,   2(q2*q3-q0*q1)   , q0^2-q1^2-q2^2+q3^2
+        q0^2 + q1^2 - q2^2 - q3^2,   2(q1 * q2 + q0 * q3)   ,   2(q1 * q3 - q0 * q2),
+          2(q1 * q2 - q0 * q3)   , q0^2 - q1^2 + q2^2 - q3^2,   2(q2 * q3 + q0 * q1),
+          2(q1 * q3 + q0 * q2)   ,   2(q2 * q3 - q0 * q1)   , q0^2 - q1^2 - q2^2 + q3^2
     )'
 end
