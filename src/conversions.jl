d5739556e7bdf85b58a7c54c7e876d1296222571
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-12-28 10:24:45 -0300

    :books: Remove comments about conversions

--- src/conversions.jl
+++ src/conversions.jl
@@ -7,14 +7,9 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-# Base.convert is already exported
-
 ################################################################################
 #                              Conversion Methods
 ################################################################################
-
-# TODO: deprecate angle_to_dcm, quat_to_dcm, ...
-# TODO: move all the implementations over here
 
 Base.convert(::Type{<:DCM}, a::EulerAngles)    = angle_to_dcm(a)
 Base.convert(::Type{<:DCM}, a::Quaternion)     = quat_to_dcm(a)
