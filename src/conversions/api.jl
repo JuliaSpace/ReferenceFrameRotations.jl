f934a9fa94e29c6e3feccccf33991da01409bf30
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-12-28 10:43:42 -0300

    :sparkles: Add conversion methods to Quaternion

--- src/conversions/api.jl
+++ src/conversions/api.jl
@@ -13,3 +13,10 @@
 Base.convert(::Type{<:DCM}, a::EulerAngles)    = angle_to_dcm(a)
 Base.convert(::Type{<:DCM}, a::Quaternion)     = quat_to_dcm(a)
 Base.convert(::Type{<:DCM}, a::EulerAngleAxis) = angleaxis_to_dcm(a)
+
+# Conversions to quaternions
+# ==============================================================================
+
+Base.convert(::Type{<:Quaternion}, a::DCM)            = dcm_to_quat(a)
+Base.convert(::Type{<:Quaternion}, a::EulerAngles)    = angle_to_quat(a)
+Base.convert(::Type{<:Quaternion}, a::EulerAngleAxis) = angleaxis_to_quat(a)
--- test/conversions/api.jl
+++ test/conversions/api.jl
@@ -23,7 +23,6 @@
         # Sample Euler angles.
         ea = EulerAngles(_rand_ang(T), _rand_ang(T), _rand_ang(T), rot_seq)
 
-        # Convert to DCM.
         D_exp = angle_to_dcm(ea)
         D_api = convert(DCM, ea)
 
@@ -38,6 +37,7 @@
     v = v / norm(v)
     a = _rand_ang(T)
     av = EulerAngleAxis(a, v)
+
     D_exp = angleaxis_to_dcm(av)
     D_api = convert(DCM, av)
 
@@ -49,6 +49,7 @@
     # Create a random quaternion.
     q = Quaternion(randn(T), randn(T), randn(T), randn(T))
     q = q / norm(q)
+
     D_exp = quat_to_dcm(q)
     D_api = convert(DCM, q)
 
@@ -65,7 +66,6 @@ end
         # Sample Euler angles.
         ea = EulerAngles(_rand_ang(T), _rand_ang(T), _rand_ang(T), rot_seq)
 
-        # Convert to DCM.
         D_exp = angle_to_dcm(ea)
         D_api = convert(DCM, ea)
 
@@ -80,6 +80,7 @@ end
     v = v / norm(v)
     a = _rand_ang(T)
     av = EulerAngleAxis(a, v)
+
     D_exp = angleaxis_to_dcm(av)
     D_api = convert(DCM, av)
 
@@ -91,8 +92,130 @@ end
     # Create a random quaternion.
     q = Quaternion(randn(T), randn(T), randn(T), randn(T))
     q = q / norm(q)
+
     D_exp = quat_to_dcm(q)
     D_api = convert(DCM, q)
 
     @test D_api === D_exp
+end
+
+# Conversion to quaternion
+# ------------------------------------------------------------------------------
+
+@testset "Julia conversion API: To Quaternion (Float 64)" begin
+    T = Float64
+
+    # Euler angles
+    # ==========================================================================
+
+    for rot_seq in valid_rot_seqs
+        # Sample Euler angles.
+        ea = EulerAngles(_rand_ang(T), _rand_ang(T), _rand_ang(T), rot_seq)
+
+        q_exp = angle_to_quat(ea)
+        q_api = convert(Quaternion, ea)
+
+        @test q_api === q_exp
+    end
+
+    # Euler angle and axis
+    # ==========================================================================
+
+    # Sample a random Euler angle and axis.
+    v = @SVector randn(T, 3)
+    v = v / norm(v)
+    a = _rand_ang(T)
+    av = EulerAngleAxis(a, v)
+
+    q_exp = angleaxis_to_quat(av)
+    q_api = convert(Quaternion, av)
+
+    @test q_api === q_exp
+
+    # Quaternion
+    # ==========================================================================
+
+    # Create a random quaternion.
+    testset = [
+        (_rand_ang(), _rand_ang2(), _rand_ang(), :Z, :Y, :X)
+        (-0.3,        +0.5,         +π,          :Z, :Y, :X)
+        (+0.3,        +0.5,         +π,          :Z, :Y, :X)
+        (+0.5,        +π,           -0.3,        :Z, :Y, :X)
+        (+0.5,        +π,           +0.3,        :Z, :Y, :X)
+        (+π,          +0.5,         -0.3,        :Z, :Y, :X)
+        (+π,          +0.5,         +0.3,        :Z, :Y, :X)
+    ]
+
+    for test in testset
+        # Unpack values in tuple.
+        a₁, a₂, a₃, r₁, r₂, r₃ = test
+
+        # Create the DCM.
+        D = angle_to_dcm(a₃, r₃) * angle_to_dcm(a₂, r₂) * angle_to_dcm(a₁, r₁)
+
+        q_exp = dcm_to_quat(D)
+        q_api = convert(Quaternion, D)
+
+        @test q_api === q_exp
+
+    end
+end
+
+@testset "Julia conversion API: To Quaternion (Float 32)" begin
+    T = Float32
+
+    # Euler angles
+    # ==========================================================================
+
+    for rot_seq in valid_rot_seqs
+        # Sample Euler angles.
+        ea = EulerAngles(_rand_ang(T), _rand_ang(T), _rand_ang(T), rot_seq)
+
+        q_exp = angle_to_quat(ea)
+        q_api = convert(Quaternion, ea)
+
+        @test q_api === q_exp
+    end
+
+    # Euler angle and axis
+    # ==========================================================================
+
+    # Sample a random Euler angle and axis.
+    v = @SVector randn(T, 3)
+    v = v / norm(v)
+    a = _rand_ang(T)
+    av = EulerAngleAxis(a, v)
+
+    q_exp = angleaxis_to_quat(av)
+    q_api = convert(Quaternion, av)
+
+    @test q_api === q_exp
+
+    # Quaternion
+    # ==========================================================================
+
+    # Create a random quaternion.
+    testset = [
+        (_rand_ang(T), _rand_ang2(T), _rand_ang(T), :Z, :Y, :X)
+        (T(-0.3),      T(+0.5),       T(+π),        :Z, :Y, :X)
+        (T(+0.3),      T(+0.5),       T(+π),        :Z, :Y, :X)
+        (T(+0.5),      T(+π),         T(-0.3),      :Z, :Y, :X)
+        (T(+0.5),      T(+π),         T(+0.3),      :Z, :Y, :X)
+        (T(+π),        T(+0.5),       T(-0.3),      :Z, :Y, :X)
+        (T(+π),        T(+0.5),       T(+0.3),      :Z, :Y, :X)
+    ]
+
+    for test in testset
+        # Unpack values in tuple.
+        a₁, a₂, a₃, r₁, r₂, r₃ = test
+
+        # Create the DCM.
+        D = angle_to_dcm(a₃, r₃) * angle_to_dcm(a₂, r₂) * angle_to_dcm(a₁, r₁)
+
+        q_exp = dcm_to_quat(D)
+        q_api = convert(Quaternion, D)
+
+        @test q_api === q_exp
+
+    end
 end
