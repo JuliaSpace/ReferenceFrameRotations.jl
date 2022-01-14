72f2feadb8a8a6d2f74c9603b651fdc2e2ef79de
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-01-14 20:55:33 -0300

    :warning: Remove deprecations

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -7,13 +7,3 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-################################################################################
-#                              Introduced in v1.0
-################################################################################
-
-@deprecate create_rotation_matrix angle_to_dcm
-
-@deprecate zeros(T::Type{Quaternion{P}}) where P zero(T)
-@deprecate zeros(T::Type{Quaternion}) zero(T)
-@deprecate zeros(q::Quaternion{T}) where T zero(q)
-
