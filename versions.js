68620cf81e5604ba74120d18f6192e6500edac02
Documenter.jl <documenter@juliadocs.github.io>
2022-01-16 15:39:08 +0000

    build based on 398cfdb

--- versions.js
+++ versions.js
@@ -1,5 +1,6 @@
 var DOC_VERSIONS = [
   "stable",
+  "v3.0",
   "v2.0",
   "v1.0",
   "v0.5",
@@ -8,5 +9,5 @@ var DOC_VERSIONS = [
   "v0.2",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v2.0.0";
+var DOCUMENTER_NEWEST = "v3.0.0";
 var DOCUMENTER_STABLE = "stable";
