87507f517dd3d3fb38c32828f1a298c3b31fbbaf
Jordan Murphy <jmurphy6895@gmail.com>
2024-12-16 13:04:26 -0700

    chore: try simplifying Zygote ext

--- ext/ReferenceFrameRotationsZygoteExt.jl
+++ ext/ReferenceFrameRotationsZygoteExt.jl
@@ -11,47 +11,10 @@ function ChainRulesCore.rrule(
 )
     y = DCM(data)
 
-    function DCM_pullback(Δ::AbstractMatrix)
+    function DCM_pullback(Δ)
         return (NoTangent(), Tuple(Δ))
     end
 
-    return y, DCM_pullback
-end
-
-function ChainRulesCore.rrule(::Type{DCM{T}}, data::SMatrix{3, 3, T}) where {T}
-    # Construct the DCM from an SMatrix
-    y = DCM(data)
-
-    function DCM_pullback(Δ)
-        # Δ could be DCM, SMatrix, MMatrix, or plain Matrix.
-        # Convert to a Matrix to avoid any reconstruction of DCM.
-        return (NoTangent(), Δ)
-    end
-    return y, DCM_pullback
-end
-
-
-function ChainRulesCore.rrule(::Type{DCM{T}}, data::MMatrix{3, 3, T}) where {T}
-    # Construct the DCM from an SMatrix
-    y = DCM(data)
-
-    function DCM_pullback(Δ)
-        # Δ could be DCM, SMatrix, MMatrix, or plain Matrix.
-        # Convert to a Matrix to avoid any reconstruction of DCM.
-        return (NoTangent(), Δ)
-    end
-    return y, DCM_pullback
-end
-
-function ChainRulesCore.rrule(::Type{DCM{T}}, data::Matrix{T}) where {T}
-    # Construct the DCM from an SMatrix
-    y = DCM(data)
-
-    function DCM_pullback(Δ)
-        # Δ could be DCM, SMatrix, MMatrix, or plain Matrix.
-        # Convert to a Matrix to avoid any reconstruction of DCM.
-        return (NoTangent(), Δ)
-    end
     return y, DCM_pullback
 end
 
