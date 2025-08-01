3eb58c5b35f38c5a01bad96e553810d681f37eb5
Jordan Murphy <jmurphy6895@gmail.com>
2025-08-01 21:57:26 +0300

    fix; modify zygote ext to handle thunks

--- ext/ReferenceFrameRotationsZygoteExt.jl
+++ ext/ReferenceFrameRotationsZygoteExt.jl
@@ -10,13 +10,14 @@ using ReferenceFrameRotations
 using ForwardDiff
 
 using Zygote.ChainRulesCore: ChainRulesCore
-import Zygote.ChainRulesCore: NoTangent
+import Zygote.ChainRulesCore: NoTangent, unthunk
 
 function ChainRulesCore.rrule(::Type{<:DCM}, data::NTuple{9, T}) where {T}
     y = DCM(data)
 
     function DCM_pullback(Δ)
-        return (NoTangent(), Tuple(Δ))
+        Δ_unthunked = unthunk(Δ)
+        return (NoTangent(), Tuple(Δ_unthunked))
     end
 
     return y, DCM_pullback
@@ -26,8 +27,9 @@ function ChainRulesCore.rrule(::typeof(orthonormalize), dcm::DCM)
     y = orthonormalize(dcm)
 
     function orthonormalize_pullback(Δ)
+        Δ_unthunked = unthunk(Δ)
         jac = ForwardDiff.jacobian(orthonormalize, dcm)
-        return (NoTangent(), reshape(vcat(Δ...)' * jac, 3, 3))
+        return (NoTangent(), reshape(vcat(Δ_unthunked...)' * jac, 3, 3))
     end
 
     return y, orthonormalize_pullback
